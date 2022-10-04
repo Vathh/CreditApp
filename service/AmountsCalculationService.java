6a1121bdf7c3822fe4144f239e50c76d1daf30a5
Maciej Przestrzelski <przestrzelski.maciej1@gmail.com>
2022-10-04 06:56:25 +0200

    implemented amounts calculation service

--- service/AmountsCalculationService.java
+++ service/AmountsCalculationService.java
@@ -1,7 +1,11 @@
 package CreditApp.service;
 
+import CreditApp.models.InputData;
+import CreditApp.models.Rate;
 import CreditApp.models.RateAmounts;
 
 public interface AmountsCalculationService {
-    public RateAmounts calculate();
+    public RateAmounts calculate(InputData inputData);
+
+    public RateAmounts calculate(InputData inputData, Rate previousRate);
 }
